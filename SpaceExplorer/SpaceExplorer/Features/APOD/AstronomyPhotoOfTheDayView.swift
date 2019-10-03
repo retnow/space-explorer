896eb55f704fa4db64e104322c807436942e1aa3
Retno Widyanti <rwidyanti@deloitte.com.au>
2019-10-03 16:28:59 +1000

    [FEAT] Added title and explanation to APOD view. [RF] Refactor astronomy photo of the day view for clarity.

---
+++ SpaceExplorer/SpaceExplorer/Features/APOD/AstronomyPhotoOfTheDayView.swift
@@ -0,0 +1,54 @@
+//
+//  AstronomyPhotoOfTheDayView.swift
+//  SwiftYouEye
+//
+//  Created by Widyanti, Retno (AU - Melbourne) on 3/10/19.
+//  Copyright © 2019 Retno Widyanti. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+import Combine
+
+struct AstronomyPhotoOfTheDayView: View {
+    @ObservedObject var loader: APODEndpointLoader
+
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 8) {
+                    Image(uiImage: loader.image ?? UIImage())
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Spacer()
+                    Text(loader.pictureOfTheDay?.title ?? "")
+                        .font(.title)
+                        .padding(EdgeInsets(
+                            top: 0,
+                            leading: 16,
+                            bottom: 0,
+                            trailing: 16))
+                        .lineLimit(nil)
+                    Text(loader.pictureOfTheDay?.explanation ?? "")
+                        .font(.body)
+                        .padding(EdgeInsets(
+                            top: 0,
+                            leading: 16,
+                            bottom: 0,
+                            trailing: 16))
+                        .lineLimit(nil)
+                }
+            }
+                .onAppear(perform: load)
+                .navigationBarTitle("Photo of the Day")
+        }
+    }
+
+    init(loader: APODEndpointLoader = APODEndpointLoader()) {
+        self.loader = loader
+    }
+
+    func load() {
+        loader.load()
+    }
+}
